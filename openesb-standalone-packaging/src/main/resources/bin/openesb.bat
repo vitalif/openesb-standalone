2402e595164da95553b72d5a712b279900e39d0b
David BRASSELY <brasseld@gmail.com>
2014-03-27 12:03:37 +0100

    OESE-23 Push arguments from command line into java process

--- openesb-standalone-packaging/src/main/resources/bin/openesb.bat
+++ openesb-standalone-packaging/src/main/resources/bin/openesb.bat
@@ -1,4 +1,4 @@
-:: ***************************************************************
+    :: ***************************************************************
 :: The contents of this file are subject to the terms
 :: of the Common Development and Distribution License
 :: (the "License").  You may not use this file except
@@ -71,7 +71,7 @@ for /r %%x in (%BOOSTRAP_PATTERN%) do (SET filename=%%x)
 if %filename%=="" Goto notfind
 
 :: Start OpenESB in a new Dos window
-%JAVA_HOME%\bin\java -Djava.util.logging.config.file=%OPENESB_HOME%/config/logger.properties -Djava.util.logging.manager=net.openesb.standalone.logger.LogManager -Djavax.net.ssl.keyStore=%OPENESB_HOME%/keystore.jks -Djavax.net.ssl.trustStore=%OPENESB_HOME%/cacerts.jks -Djavax.net.ssl.keyStorePassword=changeit -Djmx.invoke.getters=true -Dopenesb.home=%OPENESB_HOME% -jar %filename%
+%JAVA_HOME%\bin\java -Djava.util.logging.config.file=%OPENESB_HOME%/config/logger.properties -Djava.util.logging.manager=net.openesb.standalone.logger.LogManager -Djavax.net.ssl.keyStore=%OPENESB_HOME%/keystore.jks -Djavax.net.ssl.trustStore=%OPENESB_HOME%/cacerts.jks -Djavax.net.ssl.keyStorePassword=changeit -Djmx.invoke.getters=true -Dopenesb.home=%OPENESB_HOME% -jar %filename% %*
 
 GOTO endbatch
 
