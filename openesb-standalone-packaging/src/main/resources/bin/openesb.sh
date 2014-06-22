ce0b2b95042441426b6635ffd8facaf90a97f2e6
David BRASSELY <brasseld@gmail.com>
2014-06-22 22:28:08 +0200

    OESE-42 Change startup script to include jansi by default and set log handler to Jansi

--- openesb-standalone-packaging/src/main/resources/bin/openesb.sh
+++ openesb-standalone-packaging/src/main/resources/bin/openesb.sh
@@ -101,8 +101,9 @@ echo ""
 "$JAVA" $JAVA_OPTS \
         -Djava.util.logging.config.file=$OPENESB_HOME/config/logger.properties \
         -Djava.util.logging.manager=net.openesb.standalone.logger.LogManager \
+        -cp "$OPENESB_BOOT_CLASSPATH:lib/ext/jansi-1.11.jar" \
         -Djmx.invoke.getters=true \
         -Dopenesb.home=$OPENESB_HOME \
-        -jar "$OPENESB_BOOT_CLASSPATH" \
+        net.openesb.standalone.startup.Bootstrap \
         "$@"
 OPENESB_STATUS=$?
